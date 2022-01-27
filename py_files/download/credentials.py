ddf93cf8307ce7afecc9cc5c89b2bce139620468
michl-in-pan <michl-in-pan@web.de>
2022-01-27 18:37:07 +0100

    Update credentials.py

--- py_files/download/credentials.py
+++ py_files/download/credentials.py
@@ -5,7 +5,7 @@ import os
 # Radiant Earth for dataset installation
 ################################################################
 
-os.environ['MLHUB_API_KEY'] = '6fd273e083563324c317c41f4c3b6f63c46318f353317719716c8bdc0587c6c7'
+os.environ['MLHUB_API_KEY'] = ''
 
 ################################################################
 # Sentinel Hub
@@ -13,9 +13,9 @@ os.environ['MLHUB_API_KEY'] = '6fd273e083563324c317c41f4c3b6f63c46318f3533177197
 
 
 config = SHConfig()
-config.instance_id = '5e98cacf-5c35-4e3b-9674-52c8241a01f1'
-config.sh_client_id = '93c443b0-b60d-4ba3-b8e2-f05b9d5c47ac'
-config.sh_client_secret = ')!sd9B)JKEF?eemyWf*8U|93iPXo5F:#mmbw/YWM'
+config.instance_id = ''
+config.sh_client_id = ''
+config.sh_client_secret = ''
 
 
 if config.instance_id == '':
